478de5987cbd09e7088f75af18c0035207cd44b4
Sitray <ericmares13@gmail.com>
2022-10-18 11:06:47 +0200

    add global css and custom tailwind colors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import AppLayout from "../components/AppLayout/AppLayout";
+import "./Applayout.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0099ff",
+        secondary: "#1c5480",
+      },
+    },
   },
   plugins: [],
 };
