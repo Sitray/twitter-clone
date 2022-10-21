7ff3a2a510af4ed18bca87025d44539995970a24
Sitray <ericmares13@gmail.com>
2022-10-21 11:13:19 +0200

    fix userinfo bug

--- firebase/client.tsx
+++ firebase/client.tsx
@@ -1,25 +1,25 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import {
   GithubAuthProvider,
   getAuth,
   signInWithPopup,
   onAuthStateChanged,
-} from "firebase/auth";
-import IGithub from "../interfaces/IGithub";
+} from 'firebase/auth';
+import IGithub from '../interfaces/IGithub';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC_VLPjuYEIuoY-6dGQFKTHw7YKMS-BiuU",
-  authDomain: "twitter-clone-60254.firebaseapp.com",
-  projectId: "twitter-clone-60254",
-  storageBucket: "twitter-clone-60254.appspot.com",
-  messagingSenderId: "765444552557",
-  appId: "1:765444552557:web:b16129cb9e0c4872958c64",
-  measurementId: "G-FX8ZZY482R",
+  apiKey: 'AIzaSyC_VLPjuYEIuoY-6dGQFKTHw7YKMS-BiuU',
+  authDomain: 'twitter-clone-60254.firebaseapp.com',
+  projectId: 'twitter-clone-60254',
+  storageBucket: 'twitter-clone-60254.appspot.com',
+  messagingSenderId: '765444552557',
+  appId: '1:765444552557:web:b16129cb9e0c4872958c64',
+  measurementId: 'G-FX8ZZY482R',
 };
 
 // Initialize Firebase
@@ -44,7 +44,7 @@ const mapUserFromFirebase = (user: any) => {
 
 export const onAuthState = (onChange: any) => {
   return onAuthStateChanged(auth, (user) => {
-    const refactoredUser = mapUserFromFirebase(user);
+    const refactoredUser = user ? mapUserFromFirebase(user) : null;
     onChange(refactoredUser);
   });
 };
