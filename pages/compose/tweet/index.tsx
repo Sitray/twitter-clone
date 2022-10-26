5a932efa2f96d1751facc07e3d23fa2ad880b659
Sitray <ericmares13@gmail.com>
2022-10-26 11:50:33 +0200

    when the twit is sended user is redirected to /home

--- pages/compose/tweet/index.tsx
+++ pages/compose/tweet/index.tsx
@@ -1,18 +1,44 @@
+import { useRouter } from 'next/router';
 import { ChangeEvent, ChangeEventHandler, FormEvent, useState } from 'react';
 import Button from '../../../components/Button/Button';
+import { addTwit } from '../../../firebase/client';
+import { COMPOSE_STATUSES } from '../../../helpers/statuses/ComposeStatus';
 import useUser from '../../../hooks/useUser';
+import IStatuses from '../../../interfaces/IStatuses';
 
 export default function Tweet() {
   const user = useUser();
   const [message, setMessage] = useState<string>('');
+  const [status, setStatus] = useState<COMPOSE_STATUSES>(
+    COMPOSE_STATUSES.USER_NOT_KNOWN
+  );
+  const router = useRouter();
 
   const handleOnChangeText = (event: ChangeEvent<HTMLTextAreaElement>) => {
     const { value } = event.target;
+    setMessage(value);
   };
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    setStatus(COMPOSE_STATUSES.LOADING);
+    if (user) {
+      addTwit({
+        //@ts-ignore
+        avatar: user.avatar,
+        content: message,
+        //@ts-ignore
+        userId: user.uid,
+        //@ts-ignore
+        userName: user.userName,
+      }).then(() => {
+        router.push('/home');
+      });
+    }
   };
+
+  const isButtonDisabled =
+    !message.length || status === COMPOSE_STATUSES.LOADING;
 
   return (
     <>
@@ -24,9 +50,7 @@ export default function Tweet() {
           onChange={handleOnChangeText}
         ></textarea>
         <div className="p-4">
-          <Button disabled={message.length === 0 ? true : false}>
-            Twittear
-          </Button>
+          <Button disabled={isButtonDisabled}>Twittear</Button>
         </div>
       </form>
     </>
